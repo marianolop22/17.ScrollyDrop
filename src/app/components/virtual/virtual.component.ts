818c61fbdf482706fb08d9404bdaf0041858fc6e
Lopez <AndLopez@correo.local>
2019-01-14 17:41:56 -0300

    clase 292

---
+++ src/app/components/virtual/virtual.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styleUrls: ['./virtual.component.css']
+})
+export class VirtualComponent implements OnInit {
+
+
+  public personas = Array (500).fill (0);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
